51cde31acf4f0f06ec9f61774b9df6bd395776f2
Geeky Mon2 <geekymon2@gmail.com>
2021-03-13 15:33:31 +1100

    add json string enum converter

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace GeekyMon2.CarsApi.Models
 {
     public class Car
@@ -7,7 +9,11 @@ namespace GeekyMon2.CarsApi.Models
         public string Model { get; set; }
         public int Year { get; set; }
         public int Doors { get; set; }
+        
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public BodyType BodyType { get; set; }
+        
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public Transmission Transmission { get; set; }  
     }
 }
