fcc9da967178a9d9c8d986a83623a7e4b037ed42
Geeky Mon2 <geekymon2@gmail.com>
2021-04-09 14:25:21 +1000

    update

--- Cars.EF.Migrations/ConsoleStartup.cs
+++ Cars.EF.Migrations/ConsoleStartup.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 
 public class ConsoleStartup
 {
